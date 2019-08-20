bc3970cbcc90337ed3ff7e07dc945578bcf1c539
Simon Edvardsson <Simon.Edvards@gmail.com>
2019-08-20 10:48:24 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0b29212f32855754a0f4f0491b9ac10b.js"
+  "/precache-manifest.1810d7d12bcb10c415c9336680d4d409.js"
 );
 
 self.addEventListener('message', (event) => {
